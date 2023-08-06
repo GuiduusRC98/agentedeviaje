76b63f9fc313bd99c370c4ca1b889d83f735565c
Guido Pezzotta <pezzottaguido@gmail.com>
2023-08-05 21:01:55 -0300

    mejora de formulario de tickets disney

--- js/main.js
+++ js/main.js
@@ -7,3 +7,58 @@
   // Agrega un escuchador de eventos de clic al ícono de hamburguesa para llamar a la función toggleMenu()
   const hamburgerIcon = document.querySelector('.hamburger');
   hamburgerIcon.addEventListener('click', toggleMenu);
+
+
+  function increaseValue() {
+    var value = parseInt(document.getElementById('totalChildren').value, 10);
+    value = isNaN(value) ? 0 : value;
+    if (value < 10) {
+      value++;
+    }
+    document.getElementById('totalChildren').value = value;
+  }
+  
+  function decreaseValue() {
+    var value = parseInt(document.getElementById('totalChildren').value, 10);
+    value = isNaN(value) ? 0 : value;
+    if (value > 0) {
+      value--;
+    }
+    document.getElementById('totalChildren').value = value;
+  }
+
+  function increaseValueAdult() {
+    var value = parseInt(document.getElementById('totalAdults').value, 10);
+    value = isNaN(value) ? 0 : value;
+    if (value < 10) {
+      value++;
+    }
+    document.getElementById('totalAdults').value = value;
+  }
+  
+  function decreaseValueAdult() {
+    var value = parseInt(document.getElementById('totalAdults').value, 10);
+    value = isNaN(value) ? 0 : value;
+    if (value > 0) {
+      value--;
+    }
+    document.getElementById('totalAdults').value = value;
+  }
+
+  function increaseValueDias() {
+    var value = parseInt(document.getElementById('cantDias').value, 10);
+    value = isNaN(value) ? 3 : value;
+    if (value < 10) {
+      value++;
+    }
+    document.getElementById('cantDias').value = value;
+  }
+  
+  function decreaseValueDias() {
+    var value = parseInt(document.getElementById('cantDias').value, 10);
+    value = isNaN(value) ? 3 : value;
+    if (value > 3) {
+      value--;
+    }
+    document.getElementById('cantDias').value = value;
+  }
