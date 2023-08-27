cd3b3a956461d38ee77044ceb87a44edc63fa138
Guido Pezzotta <pezzottaguido@gmail.com>
2023-08-27 13:14:47 -0300

    muchos arreglos en los forms y estilos de varias cards.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
-   // Función para alternar la visualización del menú de navegación cuando se hace clic en el ícono de hamburguesa
-   function toggleMenu() {
-    const navList = document.querySelector('.nav-list');
-    navList.classList.toggle('active');
-  }
+// Esperar a que el DOM esté completamente cargado
+  document.addEventListener('DOMContentLoaded', function () {
+    // Función para alternar la visualización del menú de navegación cuando se hace clic en el ícono de hamburguesa
+    function toggleMenu() {
+      const navList = document.querySelector('.nav-list');
+      navList.classList.toggle('active');
+    }
 
-  // Agrega un escuchador de eventos de clic al ícono de hamburguesa para llamar a la función toggleMenu()
-  const hamburgerIcon = document.querySelector('.hamburger');
-  hamburgerIcon.addEventListener('click', toggleMenu);
+    // Agregar un escuchador de eventos de clic al ícono de hamburguesa para llamar a la función toggleMenu()
+    const hamburgerIcon = document.querySelector('.hamburger');
+    hamburgerIcon.addEventListener('click', toggleMenu);
+  });
 
 
   function increaseValue() {
